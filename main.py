ebfc71255a5884ef8888383fc61a27f0202a9964
myDanik <den.spesivtsev.006@gmail.com>
2024-06-30 19:32:51 +0300

    Доделан парсинг вакансий

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import fake_useragent
-
+import json
+import os
 
 
 def get_links(text):
@@ -53,23 +54,70 @@ def get_resume(link):
     soup = BeautifulSoup(data.content, "lxml")
 
     try:
-        resume = soup.find_all("div", attrs={"class":"bloko-columns-row"})[1]
-        sex = resume.find("span", attrs={"data-qa":"resume-personal-gender"}).text
-        age = int(resume.find("span", attrs={"data-qa":"resume-personal-age"}).text[:2])
-        job_search_status = resume.find("div", attrs={"class": "resume-job-search-status"}).text
-        personal_adress = resume.find("div", attrs={"class":"bloko-translate-guard"}).find("span",attrs={"data-qa": "resume-personal-address"}).text
-        position = resume.find("div", attrs={"class":"resume-block-position"}).text
-        specialization = resume.find("li", attrs={"class":"resume-block__specialization"}).text
-        busyness = resume.find("div", attrs={"class":"resume-block-container"}).find_all("p")[0].text
-        work_schedule = resume.find("div", attrs={"class":"resume-block-container"}).find_all("p")[1].text
-        experience = int(resume.find("span", attrs={"class":"resume-block__title-text resume-block__title-text_sub"}).find_all("span")[0].text[:2])*12+int(resume.find("span", attrs={"class":"resume-block__title-text resume-block__title-text_sub"}).find_all("span")[1].text[:2])
-        skills = [i.text for i in resume.find("div", attrs={"class":"bloko-tag-list"}).find_all("div", attrs={"class":"bloko-tag bloko-tag_inline"})]
-        about_me = resume.find("div", attrs={"class":"resume-block-container", "data-qa":"resume-block-skills-content"}).text
-        education = resume.find("div", attrs={"data-qa":"resume-block-education"}).find("div", attrs={"class":"bloko-columns-row"}).text
-        language = [i.text for i in resume.find("div", attrs={"data-qa":"resume-block-languages"}).find("div", attrs={"class":"resume-block-item-gap"}).find_all("div", attrs={"class":"bloko-tag bloko-tag_inline"})]
+        if len(soup.find_all("div", attrs={"class":"bloko-columns-row"}))>=2:
+            resume = soup.find_all("div", attrs={"class":"bloko-columns-row"})[1]
+        else:
+            resume = soup.find("div", attrs={"class":"bloko-columns-wrapper"})
+        if resume.find("span", attrs={"data-qa":"resume-personal-gender"}):
+            sex = resume.find("span", attrs={"data-qa":"resume-personal-gender"}).text
+        else:
+            sex = None
+        if resume.find("span", attrs={"data-qa":"resume-personal-age"}):
+            age = int(resume.find("span", attrs={"data-qa":"resume-personal-age"}).text[:2])
+        else:
+            age = 0
+        if resume.find("div", attrs={"class": "resume-job-search-status"}):
+            job_search_status = resume.find("div", attrs={"class": "resume-job-search-status"}).text
+        else:
+            job_search_status = None
+        if resume.find("div", attrs={"class":"bloko-translate-guard"}).find("span",attrs={"data-qa": "resume-personal-address"}):
+            personal_adress = resume.find("div", attrs={"class":"bloko-translate-guard"}).find("span",attrs={"data-qa": "resume-personal-address"}).text
+        else:
+            personal_adress = None
+        if resume.find("div", attrs={"class":"resume-block-position"}):
+            position = resume.find("div", attrs={"class":"resume-block-position"}).text
+        else:
+            position = None
+        if resume.find("li", attrs={"class":"resume-block__specialization"}):
+            specialization = resume.find("li", attrs={"class":"resume-block__specialization"}).text
+        else:
+            specialization = None
+        if resume.find("div", attrs={"class":"resume-block-container"}).find("p") and len(resume.find("div", attrs={"class":"resume-block-container"}).find_all("p"))==1:
+            busyness = resume.find("div", attrs={"class":"resume-block-container"}).find_all("p")[0].text
+        else:
+            busyness = None
+
+        if resume.find("div", attrs={"class":"resume-block-container"}).find("p") and len(resume.find("div", attrs={"class":"resume-block-container"}).find_all("p"))==2:
+            work_schedule = resume.find("div", attrs={"class":"resume-block-container"}).find_all("p")[1].text
+        else:
+            work_schedule = None
+        if resume.find("span", attrs={"class":"resume-block__title-text resume-block__title-text_sub"}).find("span") and len(resume.find("span", attrs={"class":"resume-block__title-text resume-block__title-text_sub"}).find_all("span")) == 2:
+            experience = int(resume.find("span", attrs={"class":"resume-block__title-text resume-block__title-text_sub"}).find_all("span")[0].text[:2])*12+int(resume.find("span", attrs={"class":"resume-block__title-text resume-block__title-text_sub"}).find_all("span")[1].text[:2])
+        elif len(resume.find("span", attrs={"class":"resume-block__title-text resume-block__title-text_sub"}).find_all("span")) == 1:
+            experience = int(resume.find("span", attrs={"class": "resume-block__title-text resume-block__title-text_sub"}).find_all("span")[0].text[:2])
+        else:
+            experience = 0
+        if resume.find("div", attrs={"class":"bloko-tag-list"}).find_all("div", attrs={"class":"bloko-tag bloko-tag_inline"}):
+            skills = [i.text for i in resume.find("div", attrs={"class":"bloko-tag-list"}).find_all("div", attrs={"class":"bloko-tag bloko-tag_inline"})]
+        else:
+            skills = []
+        if resume.find("div", attrs={"class":"resume-block-container", "data-qa":"resume-block-skills-content"}):
+            about_me = resume.find("div", attrs={"class":"resume-block-container", "data-qa":"resume-block-skills-content"}).text
+        else:
+            about_me = None
+        if resume.find("div", attrs={"data-qa":"resume-block-education"}).find("div", attrs={"class":"bloko-columns-row"}):
+            education = resume.find("div", attrs={"data-qa":"resume-block-education"}).find("div", attrs={"class":"bloko-columns-row"}).text
+        else:
+            education = None
+        if resume.find("div", attrs={"data-qa":"resume-block-languages"}).find("div", attrs={"class":"resume-block-item-gap"}).find_all("div", attrs={"class":"bloko-tag bloko-tag_inline"}):
+            language = [i.text for i in resume.find("div", attrs={"data-qa":"resume-block-languages"}).find("div", attrs={"class":"resume-block-item-gap"}).find_all("div", attrs={"class":"bloko-tag bloko-tag_inline"})]
+        else:
+            language = []
+        return language
     except Exception as e:
-        print(f"Error finding total pages: {e}")
-        return 1
+        print(f"Error: {e}")
+        return 1, link
+
 
 def get_links_vacancy(text):
     useragent = fake_useragent.UserAgent()
@@ -106,13 +154,31 @@ def get_links_vacancy(text):
             if h2_tag is not None:
                 link = h2_tag.find("a")
                 if link is not None:
-                    yield f"{link.attrs['href'].split('?')[0]}"
+                    yield "https://api." + f"{link.attrs['href'].split('?')[0]}".replace("vacancy", "vacancies", 1).split('.', 1)[1]
 
         time.sleep(1)
 def get_vacancy(link):
-    pass
+    data = requests.get(url=link, headers={"user-agent":"Parser/1.0 (den.spesivtsev.006@gmail.com)"})
+    if data.status_code != 200:
+        return 0
+    time.sleep(1)
+    vacancy = data.json()
+    name = vacancy.get("name")
+    area = vacancy.get("area").get("name")
+    if vacancy.get("salary"):
+        salary_from = vacancy.get("salary").get("from")
+        salary_to = vacancy.get("salary").get("to")
+    experience = vacancy.get("experience").get("name")
+    schedule = vacancy.get("schedule").get("name")
+    employment = vacancy.get("employment").get("name")
+    contacts = vacancy.get("contacts")
+    description = BeautifulSoup(vacancy.get("description"),"lxml").text
+    key_skills = [i.get("name") for i in vacancy.get("key_skills")]
+    driver_license = [i.get("name") for i in vacancy.get("driver_license_types")]
+    emloyer_name = vacancy.get("employer").get("name")
+    languages = [i.get("name") for i in vacancy.get("languages")]
+    return key_skills, link
 
 if __name__ == "__main__":
-    for page in get_links_vacancy("python"):
-        print(page)
-
+    for page in get_links(None):
+        print(get_resume(page))
